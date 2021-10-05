21eb213f30ce923c01256836ab85d2064f88a363
Haleksse <mignot89@gmail.com>
2021-10-04 20:49:44 -0400

    Add audio slider and quality selector
    Add : a slider a script to use the slider to change the volume.
    a dropdown and a script to change the quality of the game

---
+++ Assets/Scripts/SettingsScript/SettingsMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void ChangeVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityInt)
+    {
+        QualitySettings.SetQualityLevel(qualityInt);
+    }
+}
