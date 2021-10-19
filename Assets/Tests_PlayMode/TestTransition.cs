dcf412dd8ed076411cc17b067f0e7949278022c1
aurelien_marc <aurelien.marc@groupecimes.com>
2021-10-19 11:17:01 -0400

    Ajout de test unitaire
    Ajout d'un test pour verifier si la balle est affecter par la gravite et un test pour verifier si il y a bien la transition entre le level 2 et le level 3

--- Assets/Tests_PlayMode/TestTransition.cs
+++ Assets/Tests_PlayMode/TestTransition.cs
@@ -4,7 +4,6 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using UnityEngine.SceneManagement;
-
 public class TestTransition
 {
     // A Test behaves as an ordinary method
@@ -27,4 +26,31 @@ public class TestTransition
         yield return new WaitForSeconds(3f);
         Assert.AreEqual(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, "Menu");
     }
+    
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator SphereIsAffectByGravity() {
+        var loadSceneOperation = SceneManager.LoadSceneAsync("Assets/Scenes/level/Level2.unity");
+        loadSceneOperation.allowSceneActivation = true;
+ 
+        while (!loadSceneOperation.isDone)
+            yield return null;
+        
+        GameObject prefabRoot = GameObject.Find("Sphere");
+        prefabRoot.transform.position = new Vector3(1310.3f, 701, 0);
+        yield return new WaitForSeconds(1f);
+        Assert.AreNotEqual(prefabRoot.transform.position, new Vector3(1310.3f, 701, 0));
+    }
+    public IEnumerator TestTransitionBeetwenLevel2AndLevel3()
+    {
+        var loadSceneOperation = SceneManager.LoadSceneAsync("Assets/Scenes/level/Level2.unity");
+        loadSceneOperation.allowSceneActivation = true;
+ 
+        while (!loadSceneOperation.isDone)
+            yield return null;
+        yield return new WaitForSeconds(4f);
+        Assert.AreEqual(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, "Level3");
+    }
+    
 }
