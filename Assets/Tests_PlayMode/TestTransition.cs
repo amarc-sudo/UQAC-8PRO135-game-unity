6596836373257becd74878fe42e8dd219a49da92
Haleksse <mignot89@gmail.com>
2021-10-19 11:18:57 -0400

    Add unit test on transition in the menu
    Add :
    -a test to make sure that the playbutton change the scene to level1
    - a test to make sure that the playSettings change the scene to Settings

--- Assets/Tests_PlayMode/TestTransition.cs
+++ Assets/Tests_PlayMode/TestTransition.cs
@@ -4,19 +4,13 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
 
 public class TestTransition
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void TestTransitionSimplePasses()
-    {
-        // Use the Assert class to test conditions
-    }
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
     [UnityTest]
+    
     public IEnumerator TestTransitionEndScene()
     {
         var loadSceneOperation = SceneManager.LoadSceneAsync("Assets/Scenes/level/Level4.unity");
@@ -27,4 +21,60 @@ public class TestTransition
         yield return new WaitForSeconds(3f);
         Assert.AreEqual(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, "Menu");
     }
+    
+    [UnityTest]
+    public IEnumerator PressButtonPlayShouldLaunchFirstLevel()
+    {
+    
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        
+        
+        var loadSceneOperation = SceneManager.LoadSceneAsync("Assets/Scenes/Menu.unity");
+        loadSceneOperation.allowSceneActivation = true;
+
+        while (!loadSceneOperation.isDone)
+        {
+            yield return null;
+        }
+
+
+        var boutonplayGameObject = GameObject.Find("ButtonPlay");
+        var boutonplay = boutonplayGameObject.GetComponent<Button>();
+        Assert.NotNull(boutonplay);
+        boutonplay.onClick.Invoke();
+        yield return null;
+        yield return new WaitForSeconds(1);
+        Assert.AreEqual(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, "Level1");
+        yield return null;
+    }
+    
+    [UnityTest]
+    public IEnumerator PressButtonSettingsShouldLaunchSettingsScene()
+    {
+    
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        
+        
+        var loadSceneOperation = SceneManager.LoadSceneAsync("Assets/Scenes/Menu.unity");
+        loadSceneOperation.allowSceneActivation = true;
+
+        while (!loadSceneOperation.isDone)
+        {
+            yield return null;
+        }
+
+
+        var boutonsettingGameObject = GameObject.Find("ButtonSettings");
+        var boutonsetting = boutonsettingGameObject.GetComponent<Button>();
+        Assert.NotNull(boutonsetting);
+        boutonsetting.onClick.Invoke();
+        yield return null;
+        yield return new WaitForSeconds(1);
+        Assert.AreEqual(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, "Settings");
+        yield return null;
+    }
+    
+    
 }
